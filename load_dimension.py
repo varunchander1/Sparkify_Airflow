a1761eade43bd3e0cdc51818ed9a7c5208494108
varunchander1 <168295283+varunchander1@users.noreply.github.com>
2024-08-20 22:57:58 +0530

    Add files via upload

---
+++ load_dimension.py
@@ -0,0 +1,34 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from udacity.common import final_project_sql_statements
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 action="",
+                 query="",
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.action = action
+        self.query = query
+
+    def execute(self, context):
+        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.action == "reload":
+            self.log.info("Clearing data from destination Redshift table")
+            redshift.run("DELETE FROM {}".format(self.table))
+
+        self.log.info("Inserting data from staging table into Dim table")
+        custom_sql = self.query
+        redshift.run(custom_sql)
