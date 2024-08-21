59b73ae4c789f74ac9c02345b23199015866190c
varunchander1 <168295283+varunchander1@users.noreply.github.com>
2024-08-21 22:08:00 +0530

    Update load_dimension.py

--- Plugins/load_dimension.py
+++ Plugins/load_dimension.py
@@ -27,7 +27,7 @@ class LoadDimensionOperator(BaseOperator):
 
         if self.action == "reload":
             self.log.info("Clearing data from destination Redshift table")
-            redshift.run("DELETE FROM {}".format(self.table))
+            redshift.run("TRUNCATE TABLE {}".format(self.table))
 
         self.log.info("Inserting data from staging table into Dim table")
         custom_sql = self.query
